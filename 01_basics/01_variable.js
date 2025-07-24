5caeba74bb5a8f0b8842d1954f027e7f03ea76d3
Rajashree185 <rayrajashree9@gmail.com>
2025-07-25 01:34:26 +0530

    Container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 11667
+let accountEmail = "rajashree@gmail.com"
+var accountPass = "12345"
+accountCity =  "Jaipur"
+/*accountId = 2*/
+//console.log(accountId);
+accountEmail = "raju@gmail.com"
+accountPass = "44322"
+accountCity = "Kolkata"
+let accountState;
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
