922b42e5a0019a8f01fb22a84f6acc1b1198fbee
Rajashree185 <rayrajashree9@gmail.com>
2025-08-11 23:56:18 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treats all js code as newer version
+
+//alert(3+3) // it gives error as we are using nodejs and not browser
+
+//code readability should be high
+
+let name = "Rajashree" //string
+let age = 19 //number
+let islLoggedin = true //bolean type
+let state = null
+
+//number => range is 2 to the power 53
+//bigint is used for large numbers
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => nothing is assigned
+//symbol => for uniqueness
+
+//object
+
+console.log(typeof "Rajashree")
+console.log(typeof age)
+console.log(typeof null)// it gives the type as object
+console.log(typeof undefined)// it gives the type as undefined
